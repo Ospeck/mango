df34b6828123ab4876c4feec8edf5bb7113c9cbb
Ospeck <pandrospeck@gmail.com>
2018-04-06 05:14:09 -0500

    grr

---
+++ src/main/java/com/controladores/Main.java
@@ -0,0 +1,28 @@
+package com.controladores;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        Login login = new Login(primaryStage);
+        Scene scene = new Scene(login,707, 481);
+
+
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Chino Jr");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
+
